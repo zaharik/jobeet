a9a184fa6ae713541de179a074ba0bf9dd87e506
zaharik <s_zaharik@ukr.net>
2011-11-21 11:11:29 +0200

    Day 6. Full

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -12,7 +12,27 @@
  */
 class JobeetJob extends BaseJobeetJob
 {
+    public function save(Doctrine_Connection $conn = NULL){
+        if ($this->isNew() && !$this->getExpiresAt()){
+            $now = $this->getCreatedAt() ? $this->getDateTimeObject('created_at')->format('U') : time();
+            $this->setExpiresAt(date('Y-m-d H:i:s', $now + 86400 * sfConfig::get('app_active_days')));
+        }
+        return parent::save($conn);
+    }
+
     public function __toString() {
         return sprintf('%s at %s ($s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+    }
+    
+    public function getCompanySlug() {
+        return Jobeet::slugify($this->getCompany());
+    }
+    
+    public function getPositionSlug() {
+        return Jobeet::slugify($this->getPosition());
+    }
+    
+    public function getLocationSlug() {
+        return Jobeet::slugify($this->getLocation());
     }
 }
