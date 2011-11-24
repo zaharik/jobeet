a812131efc837f64b4910210e57084d4ab6392b1
zaharik <s_zaharik@ukr.net>
2011-11-24 17:44:13 +0200

    Commit #134

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -9,10 +9,6 @@
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 
-
-
-
-
 class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
